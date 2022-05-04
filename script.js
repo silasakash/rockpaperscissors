e047866dffcfce98d77406b8d20dc905769ecdbc
akashsilas <akashsilas1@gmail.com>
2022-05-04 23:23:35 +0530

    Make changes to index.html and script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function computerPlay() {
+    const  list1 = ['Rock','Paper','Scissors'];
+    let randomPick = list1[Math.floor(Math.random() * list1.length)];
+    return randomPick;
+}
+
+function round(playerSelection,computerSelection) {
+    
+}
