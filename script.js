de015971f56c8f207df42542071149fc497d143c
akashsilas <akashsilas1@gmail.com>
2022-05-06 21:01:18 +0530

    Modify script to create a simple browser game

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let playerScore = 0;
+let computerScore = 0;
 function computerPlay() {
     const  list1 = ['rock','paper','scissors'];
     let randomPick = list1[Math.floor(Math.random() * list1.length)];
@@ -9,37 +11,37 @@ function playRound(playerSelection,computerSelection) {
     playerSelection = selection.toLowerCase();
     computerSelection = computerPlay();
     if (playerSelection == computerSelection) {
-        playerScore = playerScore++;
-        computerScore = computerScore++;
-        return "It's a tie!"
+        playerScore = ++playerScore;
+        computerScore = ++computerScore;
+        return "It's a Tie!"
     }
     else if(playerSelection == "rock") {
         if (computerSelection == "paper") {
-            computerScore = computerScore++;
+            computerScore = ++computerScore;
             return "You Lose! Paper beats Rock"
         }
         else if (computerSelection == "scissors") {
-            playerScore = playerScore++;
+            playerScore = ++playerScore;
             return "You Win! Rock beats Scissors"
         }
     }
     else if(playerSelection == "paper") {
         if (computerSelection == "rock") {
-            playerScore = playerScore++;
+            playerScore = ++playerScore;
             return "You Win! Paper beats Rock"
         }
         else if (computerSelection == "scissors") {
-            computerScore = computerScore++;
+            computerScore = ++computerScore;
             return "You Lose! Scissors beats Paper"
         }
     }
     else if(playerSelection == "scissors") {
         if (computerSelection == "paper") {
-            playerScore = playerScore++;
+            playerScore = ++playerScore;
             return "You Win! Scissors beats Paper"
         }
         else if (computerSelection == "rock") {
-            computerScore = computerScore++;
+            computerScore = ++computerScore;
             return "You Lose! Rock beats Scissors"
         }
     }
@@ -49,7 +51,23 @@ function playRound(playerSelection,computerSelection) {
 }
 
 function game() {
-    let playerScore = 0;
-    let computerScore = 0;
-
+    playerScore = 0;
+    computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        console.log(playRound());
+        console.log("Player Score = " + playerScore);
+        console.log("Computer Score = " + computerScore);
+    }
+    if (playerScore == computerScore) {
+        return "It's a Tie"
+    }
+    else if (playerScore > computerScore) {
+        return "You Win!"
+    }
+    else if (playerScore < computerScore) {
+        return "You Lose!"
+    }
+    else {
+        return "Something Went Wrong!"
+    }
 }
