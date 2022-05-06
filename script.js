4dbbc7b2617621f6ab68d879016ec2bd92ceeae6
akashsilas <akashsilas1@gmail.com>
2022-05-06 20:23:00 +0530

    Create playRound function

--- script.js
+++ script.js
@@ -1,9 +1,55 @@
 function computerPlay() {
-    const  list1 = ['Rock','Paper','Scissors'];
+    const  list1 = ['rock','paper','scissors'];
     let randomPick = list1[Math.floor(Math.random() * list1.length)];
     return randomPick;
 }
 
-function round(playerSelection,computerSelection) {
-    
+function playRound(playerSelection,computerSelection) {
+    selection = prompt("Rock,paper or Scissors?");
+    playerSelection = selection.toLowerCase();
+    computerSelection = computerPlay();
+    if (playerSelection == computerSelection) {
+        playerScore = playerScore++;
+        computerScore = computerScore++;
+        return "It's a tie!"
+    }
+    else if(playerSelection == "rock") {
+        if (computerSelection == "paper") {
+            computerScore = computerScore++;
+            return "You Lose! Paper beats Rock"
+        }
+        else if (computerSelection == "scissors") {
+            playerScore = playerScore++;
+            return "You Win! Rock beats Scissors"
+        }
+    }
+    else if(playerSelection == "paper") {
+        if (computerSelection == "rock") {
+            playerScore = playerScore++;
+            return "You Win! Paper beats Rock"
+        }
+        else if (computerSelection == "scissors") {
+            computerScore = computerScore++;
+            return "You Lose! Scissors beats Paper"
+        }
+    }
+    else if(playerSelection == "scissors") {
+        if (computerSelection == "paper") {
+            playerScore = playerScore++;
+            return "You Win! Scissors beats Paper"
+        }
+        else if (computerSelection == "rock") {
+            computerScore = computerScore++;
+            return "You Lose! Rock beats Scissors"
+        }
+    }
+    else {
+        return "Enter a valid input!"
+    }
+}
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+
 }
